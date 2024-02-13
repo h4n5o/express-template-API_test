d34d3affcbda8387d2a09b44e666f0f8573031fb
h4n5o <72876852+h4n5o@users.noreply.github.com>
2024-02-13 10:39:21 +0100

    ...

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+
+const { PORT } = process.env;
+const app = express();
+
+app.use(cors());
+
+const profiles = [
+  {
+    id: 1, firstname: "Jolly", birthdate: new Date("1990-10-10")
+  }
+];
+
+app.get(
+  "/test", (req, res) => {
+    res.send("Hollariiiitüüüü!");
+  }
+);
+
+app.get(
+  "/profile", (req, res) => {
+    res.json({ profile: { name: "Igor" } });
+  }
+);
+
+app.get('/user', (req, res) => {
+  const user = {
+    firstName: 'Igor',
+    lastName: 'Geheimmann',
+    address: 'Beispielstraße 1, 12345 Musterstadt',
+    hobbies: ['Springen', 'Tanzen', 'Klatschen']
+  };
+  res.json(user);
+});
+
+app.get('/todos', (req, res) => {
+  const todos = [
+    {
+      id: 1,
+      task: 'Einkaufen gehen',
+      completed: false
+    },
+    {
+      id: 2,
+      task: 'Füße waschen',
+      completed: true
+    }
+  ];
+  res.json(todos);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
